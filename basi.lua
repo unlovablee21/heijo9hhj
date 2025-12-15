66ece112cdedf35ae22db930013f3c69b2fc27bf
boss <167646835+unlovablee21@users.noreply.github.com>
2025-12-15 04:47:20 +0100

    Update basi.lua

--- basi.lua
+++ basi.lua
@@ -193,7 +193,7 @@ TextChatService.OnIncomingMessage = function(message)
 	if command == "!kick" then
 		local reason = table.concat(args, " ", 3)
 		if reason == "" then
-			reason = "You were kicked by an admin."
+			reason = "You were kicked by 0uwy"
 		end
 		target:Kick(reason)
 	end
